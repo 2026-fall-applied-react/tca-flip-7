0b7bbdb8c3c9c30ea978e58ff31bdf3a8122f180
tsteelematc <tsteele@madisoncollege.edu>
2026-09-16 19:27:35 -0500

    learned accordion, and py-0 px-1 for card-body ! ! !

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,34 @@ function App() {
       </div>
 
 
+
+
+      <div className="card w-full bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body py-0 px-1">
+          <h2 className="card-title">Accordion</h2>
+
+
+
+          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" defaultChecked />
+            <div className="collapse-title font-semibold">How do I create an account?</div>
+            <div className="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
+            <div className="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
+          </div>
+          <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title font-semibold">How do I update my profile information?</div>
+            <div className="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
+          </div>
+
+        </div>
+      </div>
+
+
     </div>
   )
 }
