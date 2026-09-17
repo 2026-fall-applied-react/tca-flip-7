fe0493487cd380af682cfaf9436d97a61b5bb6ed
tsteelematc <tsteele@madisoncollege.edu>
2026-09-16 20:26:24 -0500

    navigation working, boom ! ! !

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,22 +1,22 @@
 import { useNavigate } from "react-router";
 
-export const Home = () => {
+export const Setup = () => {
 
     const nav = useNavigate();
 
     return (
         <div>
             <h1>
-                Home
+                Setup
             </h1>
 
             <button 
                 className="btn btn-soft btn-lg mt-3"
                 onClick={
-                    () => nav('/setup')
+                    () => nav('/play')
                 }
             >
-                Setup a Game
+                Play the Game
             </button>
         </div>
     );
