b15dd01e09a3b3e385dda58fdd5b585d7c52c8e1
tsteelematc <tsteele@madisoncollege.edu>
2026-09-16 19:20:53 -0500

    daisy ui step component kinda neat, maybe use ? ? ?

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,19 @@ function App() {
         </div>
       </div>
 
+      <div className="card w-full bg-base-100 card-lg shadow-sm my-3">
+        <div className="card-body">
+          <h2 className="card-title">Step ? ? ?</h2>
 
+          <ul className="steps steps-vertical">
+            <li className="step step-primary">Register</li>
+            <li className="step step-primary">Choose plan</li>
+            <li className="step">Purchase</li>
+            <li className="step">Receive Product</li>
+          </ul>
+
+        </div>
+      </div>
 
 
     </div>
